7507b5ae48da354f3b054da24d32d5a0245258ec
giselly <giselly.avp@gmail.com>
2024-03-01 12:02:27 -0300

    :ok_hand: feat: Esqueleto de eventos do bottom-content e estilos

--- jss/modules/slider.js
+++ jss/modules/slider.js
@@ -1,6 +1,39 @@
 export default class Slider {
     constructor() {
+        this.slide = document.querySelector(".product-list");
+        this.wrapper = document.querySelector(".slide-wrapper");
+
       
+        this.onStart = this.onStart.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
     }
-  }
-  
+
+    onStart(event) {
+        event.preventDefault();
+        console.log('mouseDown');
+        this.wrapper.addEventListener('mousemove', this.onMove);
+    }
+
+    onMove(event) {
+        console.log('moveu');
+    }
+
+    onEnd(event) {
+        console.log('acabou');
+        this.wrapper.removeEventListener('mousemove', this.onMove);
+    }
+
+    addSlideEvents() {
+        this.wrapper.addEventListener('mousedown', this.onStart);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
+    }
+
+    init() {
+        this.addSlideEvents();
+        return this;
+    }
+}
+
+const slider = new Slider();
+slider.init();
